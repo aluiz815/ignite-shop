4385f3a8967deeff9a87ae2dc4bd2b8c72b94a36
Andre Luiz <aluiz815@gmail.com>
2022-10-29 18:24:04 -0300

    Project Finished

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+
+globalStyles()
+import logoImg from '../assets/logo.svg'
+import Image from "next/image"
+import { Container, Header } from "../styles/pages/app"
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt=""/>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
-export default MyApp
